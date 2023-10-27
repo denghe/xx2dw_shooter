e20a2166027900d5c183f165c7793c3422f3ac2d
denghe <denghe@hotmail.com>
2023-10-27 14:55:06 +0800

    add d14 for scroll view

---
+++ d14/pch.cpp
@@ -0,0 +1,41 @@
+#include <pch.h>
+#include <game_looper.h>
+
+
+#ifndef __EMSCRIPTEN__
+
+#define GLAD_MALLOC(sz)       malloc(sz)
+#define GLAD_FREE(ptr)        free(ptr)
+#define GLAD_GL_IMPLEMENTATION
+#include <glad.h>
+
+//#define STBI_NO_JPEG
+//#define STBI_NO_PNG
+#define STBI_NO_GIF
+#define STBI_NO_PSD
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#define STBI_NO_HDR
+#define STBI_NO_TGA
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#endif
+
+
+GameLooper gLooper;
+
+#ifdef __EMSCRIPTEN__
+int32_t main() {
+	emscripten_request_animation_frame_loop([](double ms, void*)->EM_BOOL {
+		return gLooper.JsLoopCallback(ms);
+	}, nullptr);
+}
+#else
+int32_t main() {
+	gLooper.showFps = true;
+	gLooper.title = "xx2dw_d14";
+	gLooper.Init();
+	gLooper.Run();
+}
+#endif
---
+++ d14/scene_main_menu.cpp
@@ -0,0 +1,11 @@
+﻿#include <pch.h>
+#include <all.h>
+
+void SceneMainMenu::Init() {
+	rootNode.Emplace()->Init();
+
+}
+
+void SceneMainMenu::Draw() {
+	gLooper.DrawNode(rootNode);
+};
