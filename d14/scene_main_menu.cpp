d78517f50b81d88670940f083678b5f2947bdca1
denghe <denghe@hotmail.com>
2023-10-31 18:58:44 +0800

    minor

--- d14/scene_main_menu.cpp
+++ d14/scene_main_menu.cpp
@@ -24,10 +24,10 @@ void SceneMainMenu::Init() {
 	rootNode.Emplace()->Init();
 
 	auto sv = rootNode->MakeChildren<ScrollView>();
-	sv->Init(3, { -100, 100 }, { 200, 200 }, { 0.5f, 0.5f }, {2, 2});
+	sv->Init(2, { -100, 100 }, { 200, 200 }, { 0.5f, 0.5f }, {2, 2});
 
 	sv->MakeChildren<Scale9Sprite>()->Init(1, {}, {}, sv->size, gLooper.s9cfg_panel);
-	sv->MakeChildren<SVContent>()->Init(2, {}, sv->size, {});
+	sv->MakeChildren<SVContent>()->Init(3, {}, sv->size, {});
 
 	sv->MakeContent<RichLabel>()->Init(4, {}, { 1,1 }, {}, sv->size.x)
 		.AddText(U" asdfasdfasd f sdf sadf sdf sd fs adf asdf sa fds df s df.\n")
