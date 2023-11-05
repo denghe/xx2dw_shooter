300dc0933546ce3637814eac2386603364689367
denghe <denghe@hotmail.com>
2023-11-05 20:46:23 +0800

    add d15 empty project

---
+++ d15/scene_main_menu.cpp
@@ -0,0 +1,17 @@
+﻿#include <pch.h>
+#include <all.h>
+
+struct PropertiesPanel : Node {
+	virtual void Draw() override {
+	}
+};
+
+void SceneMainMenu::Init() {
+	rootNode.Emplace()->Init();
+
+
+}
+
+void SceneMainMenu::Draw() {
+	gLooper.DrawNode(rootNode);
+};
