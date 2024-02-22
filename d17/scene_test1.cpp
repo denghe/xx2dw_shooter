1280a8d11edc70c0e80bad45fe7bf4576392e8cd
denghe <denghe@hotmail.com>
2024-02-22 17:29:14 +0800

    add new proj d17 for simulate bag item drag

---
+++ d17/scene_test1.cpp
@@ -0,0 +1,35 @@
+﻿#include <pch.h>
+#include <all.h>
+
+#pragma region SceneTest1
+
+void SceneTest1::Init() {
+	camera.SetMaxFrameSize({ gCfg.cellSize, gCfg.cellSize });
+	camera.SetOriginal(gCfg.gridCenterPos);
+	camera.SetScale(1.f);
+
+	// todo
+}
+
+void SceneTest1::Update() {
+	camera.Calc();
+
+	// scale control
+	if (gLooper.KeyDownDelay(KeyboardKeys::Z, 0.02f)) {
+		camera.IncreaseScale(0.1f, 5);
+	} else if (gLooper.KeyDownDelay(KeyboardKeys::X, 0.02f)) {
+		camera.DecreaseScale(0.1f, 0.1f);
+	}
+
+	// todo: mouse drag item
+}
+
+void SceneTest1::Draw() {
+	camera.Calc();
+
+	// todo: draw background grid lines ?
+
+	// todo
+};
+
+#pragma endregion
---
+++ d17/scene_test1.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <game_looper.h>
+
+// todo: simulate bag -> equip item drag
+
+struct Cfg {
+	static constexpr float cellSize{ 32 };
+
+	static constexpr int numRows{ 512 };
+	static constexpr int numCols{ 512 };
+
+	static constexpr XY gridSize{ numCols * cellSize, numRows * cellSize };
+	static constexpr XY gridSize_2{ gridSize.x / 2, gridSize.y / 2 };
+	static constexpr XY gridCenterPos{ gridSize_2 };
+
+	static constexpr float mouseHitRange{ 50.f };
+};
+inline Cfg gCfg;
+
+
+
+struct SceneTest1 : Scene {
+	Camera camera;
+
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Draw() override;
+};
