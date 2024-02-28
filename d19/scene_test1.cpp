2a693b9101440f6c94e07bf209ccf49b4e0441a6
denghe <denghe@hotmail.com>
2024-02-29 02:21:25 +0800

    writing...

--- d19/scene_test1.cpp
+++ d19/scene_test1.cpp
@@ -3,20 +3,84 @@
 
 #pragma region Eye
 
-void Eye::Init(BigEye& owner_, xx::FromTo<float> const& cRadiansRange_, xx::FromTo<float> const& cDistanceRange_) {
-	// todo
+void Eye::Init(EyeBase& prev_, xx::FromTo<float> const& cRadiansRange_, xx::FromTo<float> const& cDistanceRange_) {
+	// init args
+	prev = &prev_;
+	cRadiansRange = cRadiansRange_;
+	cDistanceRange = cDistanceRange_;
+	scale = cScale;
+	radius = cRadius;
+	radians = prev->radians;
+	pos = prev->pos;
+	Update();
 }
 
-void Eye::Init(Eye& prev_, xx::FromTo<float> const& cRadiansRange_, xx::FromTo<float> const& cDistanceRange_) {
-	// todo
+// limit a by from ~ to
+// no change: return false
+inline XX_FORCE_INLINE bool Limit(float& a, float from, float to) {
+	// -PI ~~~~~~~~~~~~~~~~~~~~~~~~~ a ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PI
+    assert(a >= gNPI && a <= gPI);
+	// -PI ~~~~~~~~ from ~~~~~~~~~~~~~~ a ~~~~~~~~~~~~~~~~ to ~~~~~~~~~~~~ PI
+	if (a >= from && a <= to) return false;
+	// from ~~~~~~~~~~~~~~~ -PI ~~~~~~~~~~~~~~~~~~ to ~~~~~~~~~~~~~~~ PI
+	if (from < gNPI) {
+		// ~~~~~~ from ~~~~~~~ -PI ~~~~~~ to ~~~~ a ~~~~ 0 ~~~~~~~~~~~~~ PI
+		if (a < 0) {
+			if (a - to < from + g2PI - a) {
+				a = to;
+			} else {
+				a = from + g2PI;
+			}
+		// ~~~~~ d ~~~~~~ from ~~~~~~~ -PI ~~~~~~~~ to ~~~~~~~~ 0 ~~~~~~~ a ~~~~ PI
+		} else {
+			auto d = a - g2PI;
+			if (d >= from && d <= to) return false;
+			else {
+				if (from - d < a - to) {
+					a = from + g2PI;
+				} else {
+					a = to;
+				}
+			}
+		}
+	// -PI ~~~~~~~~~~ from ~~~~~~~~~~~~~ PI ~~~~~~~ to
+	} else if (to > gPI) {
+		// -PI ~~~~~~~~~~~~~~~ 0 ~~~~~ a ~~~~~ from ~~~~~~ PI ~~~~~~~ to
+		if (a > 0) {
+			if (from - a < a - (to - g2PI)) {
+				a = from;
+			} else {
+				a = to - g2PI;
+			}
+		// -PI ~~~~~~~ a ~~~~~~~~ 0 ~~~~~~~ from ~~~~~~ PI ~~~~~~~ to ~~~~~ d ~~~~~
+		} else {
+			auto d = a + g2PI;
+			if (d >= from && d <= to) return false;
+			else {
+				if (from - a < d - to) {
+					a = from;
+				} else {
+					a = to - g2PI;
+				}
+			}
+		}
+	} else {
+		// -PI ~~~~~ a ~~~~ from ~~~~~~~~~~~~~~~~~~ to ~~~~~~~~~~~ PI
+		if (a < from) {
+			a = from;
+		// -PI ~~~~~~~~~ from ~~~~~~~~~~~~~~~~~~ to ~~~~~ a ~~~~~~ PI
+		} else {
+			a = to;
+		}
+	}
+    return true;	// todo: bug fix. handle: to - from > PI?
 }
 
-void Eye::Update(BigEye& owner_) {
-	// todo
-}
-
-void Eye::Update(Eye& prev_) {
-	// todo
+void Eye::Update() {
+	radians = std::atan2(pos.y - prev->pos.y, pos.x - prev->pos.x);
+	Limit(radians, prev->radians + cRadiansRange.from, prev->radians + cRadiansRange.to);
+	pos.x = prev->pos.x + std::cos(radians) * cDistanceRange.from;
+	pos.y = prev->pos.y + std::sin(radians) * cDistanceRange.from;
 }
 
 void Eye::Draw(Camera const& camera) {
@@ -34,22 +98,45 @@ void Eye::Draw(Camera const& camera) {
 #pragma region BigEye
 
 void BigEye::Init(XY const& pos_) {
+	// init args
 	pos = {};
 	scale = cScale;
 	radius = cRadius;
 	radians = {};
 
-	// todo: fill children
+	// init children
+	static constexpr int numEyess{ 20 }, numEyes{ 30 };
+	static constexpr float step{ g2PI / numEyess };
+	eyess.Resize(numEyess);
+	for (int i = 0; i < numEyess; i++) {
+		auto& eyes = eyess[i];
+		eyes.Resize(numEyes);
+		auto r = step * i;
+		eyes[0].Init(*this, {r, r}, { radius, radius });	// todo
+		for (int j = 1; j < numEyes; j++) {
+			eyes[j].Init(eyes[j - 1], {-0.2f, 0.2f}, { Eye::cRadius, Eye::cRadius });	// todo
+		}
+	}
 }
 
 void BigEye::Update(Camera const& camera) {
-	// todo: fix drag control
 	auto& m = gLooper.mouse;
 	if (m.btnStates[0] && !gLooper.mouseEventHandler) {
+		// follow mouse
 		auto p = camera.ToLogicPos(m.pos);
-		auto d = p - pos;
-		if (d.x * d.x + d.y * d.y < radius * radius) {
+		auto v = p - pos;
+		auto distance = std::sqrt(v.x * v.x + v.y * v.y);
+		if (auto d = distance - cSpeed; d > 0) {
+			auto inc = v / distance * cSpeed;
+			pos = pos + inc;
+		} else {
 			pos = p;
+		}
+	}
+
+	for (auto& eyes : eyess) {
+		for (auto& eye : eyes) {
+			eye.Update();
 		}
 	}
 }
--- d19/scene_test1.h
+++ d19/scene_test1.h
@@ -10,23 +10,23 @@ struct EyeBase {
 struct BigEye;
 struct Eye : EyeBase {
 	static constexpr float cScale{ 1 };
-	static constexpr float cRadius{ cScale * 8 };
+	static constexpr float cRadius{ cScale * 7 };
 
-	xx::FromTo<float> cDistanceRange{ 3.f, 6.f };
-	xx::FromTo<float> cRadiansRange{ 3.f, 6.f };
+	EyeBase* prev{};
+	xx::FromTo<float> cDistanceRange{};
+	xx::FromTo<float> cRadiansRange{};
 
-	void Init(BigEye& owner_, xx::FromTo<float> const& cRadiansRange_, xx::FromTo<float> const& cDistanceRange_);
-	void Init(Eye& prev_, xx::FromTo<float> const& cRadiansRange_, xx::FromTo<float> const& cDistanceRange_);
+	void Init(EyeBase& prev_, xx::FromTo<float> const& cRadiansRange_, xx::FromTo<float> const& cDistanceRange_);
 
-	void Update(BigEye& owner_);
-	void Update(Eye& prev_);
+	void Update();	// todo: args total radians
 
 	void Draw(Camera const& camera);
 };
 
 struct BigEye : EyeBase {
 	static constexpr float cScale{ 30 };
-	static constexpr float cRadius{ cScale * 8 };
+	static constexpr float cRadius{ cScale * 7 };
+	static constexpr float cSpeed{ 3000.f / gDesign.fps };
 
 	xx::Listi32<xx::Listi32<Eye>> eyess;
 
