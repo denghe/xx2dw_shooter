fbffb83005d3590026ed3a208bd1901240976d70
denghe <denghe@hotmail.com>
2024-03-01 17:18:38 +0800

    writing...

--- d21/scene_test1.cpp
+++ d21/scene_test1.cpp
@@ -1,35 +1,286 @@
 ﻿#include <pch.h>
 #include <all.h>
 
+#pragma region PhysSceneItem
+
+void PhysSceneItem::PhysAdd() {
+	SGCAdd(scene->sgcPhysItems, pos);
+}
+
+void PhysSceneItem::PhysUpdate() {
+	SGCUpdate(pos);
+}
+
+void PhysSceneItem::PhysRemove() {
+	SGCRemove();
+}
+
+bool PhysSceneItem::PhysExists() {
+	return !!_sgc;
+}
+
+PhysSceneItem::~PhysSceneItem() {
+	SGCRemove();
+}
+
+#pragma endregion
+
+#pragma region Staff
+
+xx::Task<> Staff::MainTask() {
+	while (true) {
+		// follow owner( sync pos )
+		if (owner) {
+			pos = owner->pos + XY{ 0, 0.1f };
+		}
+
+		// try to toward mouse
+		auto mPos = scene->camera.ToLogicPos(gEngine->mouse.pos);
+		auto d = mPos - pos;
+		auto r = std::atan2(d.y, d.x);
+		RotateControl::Step(radians, r, cFrameMaxChangeRadians);
+
+		// calc fire pos by current radians
+		auto c = std::cos(radians);
+		auto s = std::sin(radians);
+		firePos = pos + XY{ c, s } *cFireDistance;	// * scale
+
+		// fire
+		if (gLooper.mouse.btnStates[0] && nextFireSecs <= gLooper.nowSecs) {
+			nextFireSecs = (float)gLooper.nowSecs + cFireDelaySecs;
+
+			im->Create<Bullet>(xx::WeakFromThis(this));
+		}
+
+		co_yield 0;
+	}
+}
+
+void Staff::Init(ItemManagerBase* im_, xx::Weak<SceneItem> owner_) {
+	assert(owner_);
+	SceneItemInit(cTypeId, im_);
+	owner = std::move(owner_);
+
+	// tasks init
+	mainTask = MainTask();
+	// ...
+
+	// born init
+	//drawOffset = cDrawOffset;
+	pos = owner->pos + XY{ 0, 0.1f };
+	radians = (float)-M_PI_2;
+}
+
+bool Staff::Update() {
+	return mainTask.Resume();
+}
+
+void Staff::Draw(Camera const& camera) {
+	auto& q = Quad::DrawOnce(gLooper.frames_staff[cFrameIndex]);
+	q.pos = camera.ToGLPos(pos);
+	q.anchor = cAnchor;
+	q.scale = XY{ flipX ? -camera.scale : camera.scale, camera.scale } *scale;
+	q.radians = radians;
+	q.colorplus = 1;
+	q.color = { 255, 255, 255, 255 };
+}
+
+#pragma endregion
+
+#pragma region Bullet
+
+xx::Task<> Bullet::MainTask() {
+	// move. in life cycle
+	for (int e = gLooper.frameNumber + cLifeNumFrames; gLooper.frameNumber < e;) {
+		if (moveTask.Resume()) co_return;
+		co_yield 0;
+	}
+
+	// move + fadeout
+	auto scaleBak = scale;
+	auto radiusBak = radius;
+	for (float s = 1; s > 0; s -= 1.f / gDesign.fps) {
+		scale = scaleBak * s;
+		radius = radiusBak * s;
+		if (moveTask.Resume()) co_return;
+		co_yield 0;
+	}
+	scale = {};
+	radius = 0;
+}
+
+xx::Task<> Bullet::MoveTask() {
+	XY inc{ std::cos(radians) * cSpeed, std::sin(radians) * cSpeed };
+	while (true) {
+		pos += inc;
+		//// hit check
+		//if (auto r = FindNeighborCross(scene->sgcPhysItems, pos, radius)) {
+		//	r->Hit(*this, damage);
+		//	// find nearest xxx & shoot Bullet 1
+		//	if (auto t = FindNearest(scene->sgcPhysItems, gLooper.sgrdd, pos, Bullet1::cAttackRange)) {
+		//		im->Create<Bullet1>(owner, pos, radians, xx::WeakFromThis(t), 5);
+		//	}
+		//	co_return;
+		//}
+		co_yield 0;
+	}
+}
+
+void Bullet::Init(ItemManagerBase* im_, xx::Weak<Staff> weapon_) {
+	assert(weapon_);
+	SceneItemInit(cTypeId, im_);
+	owner = weapon_->owner;
+
+	// tasks init
+	mainTask = MainTask();
+	moveTask = MoveTask();
+	// ...
+
+	// get weapon args for born init
+	pos = weapon_->firePos;
+	radians = weapon_->radians;
+	scale = { cScale, cScale };
+	// damage = weapon_->damage; ? cDamage ? buff calculate ?
+	radius = cRadius;
+}
+
+bool Bullet::Update() {
+	return mainTask.Resume();
+}
+
+void Bullet::Draw(Camera const& camera) {
+	auto& q = Quad::DrawOnce(gLooper.frames_bullet[0]);
+	q.pos = camera.ToGLPos(pos);
+	q.anchor = cAnchor;
+	q.scale = XY{ flipX ? -camera.scale : camera.scale, camera.scale } *scale;
+	q.radians = radians;
+	q.colorplus = 1;
+	q.color = { 255, 255, 255, 255 };
+
+	if (scene->isBorderVisible) {
+		LineStrip().FillCirclePoints({}, radius, {}, 20, scale * camera.scale)
+			.SetPosition(camera.ToGLPos(pos))
+			.Draw();
+	}
+}
+
+
+#pragma endregion
+
+#pragma region Human
+
+void Human::Init(ItemManagerBase* im_) {
+	SceneItemInit(cTypeId, im_);
+	scene->human = xx::WeakFromThis(this);
+
+	// tasks init
+	mainTask = MainTask();
+	idleTask = IdleTask();
+	// ...
+
+	// born init
+	pos = gCfg.mapCenterPos;
+	speed = cSpeed;
+	radius = cRadius;
+	SetDirection(MoveDirections::Down);
+
+	// weapon init
+	im->Create<Staff>(xx::WeakFromThis(this));
+}
+
+bool Human::Update() {
+	return mainTask.Resume();
+}
+
+xx::Task<> Human::IdleTask() {
+	while (true) {
+		for (scale.y = cIdleScaleYTo; scale.y > cIdleScaleYFrom; scale.y -= cIdleScaleYStep) co_yield 0;
+		for (scale.y = cIdleScaleYFrom; scale.y < cIdleScaleYTo; scale.y += cIdleScaleYStep) co_yield 0;
+	}
+}
+
+XY Human::GetPhysPos() {
+	return pos + XY{ 0, -radius / 2 };
+}
+
+void Human::SetDirection(MoveDirections d) {
+	direction = d;
+	int i;
+	if (xx::FlagContains(d, MoveDirections::Down)) {
+		i = 0;
+	} else if (xx::FlagContains(d, MoveDirections::Up)) {
+		i = 3;
+	} else if (d == MoveDirections::Left) {
+		i = 1;
+	} else {
+		i = 2;
+	}
+	frameIndexFrom = cFrameIndexRanges[i];
+	frameIndexTo = cFrameIndexRanges[i + 1];
+	frameIndex = frameIndexFrom;
+}
+
+xx::Task<> Human::MainTask() {
+	while (true) {
+		// keyboard move control
+		if (auto r = gLooper.GetKeyboardMoveInc(); r.has_value()) {
+			pos += r->second * speed;
+			if (direction != r->first) {
+				SetDirection(r->first);
+			}
+			FrameControl::Forward(frameIndex, cFrameInc * speed, frameIndexFrom, frameIndexTo);
+		} else {
+			idleTask();
+		}
+		scene->camera.SetOriginal(pos);
+
+		co_yield 0;
+	}
+}
+
+void Human::Draw(Camera const& camera) {
+	auto& q = Quad::DrawOnce(gLooper.frames_human_1[(int)frameIndex]);
+	q.pos = camera.ToGLPos(pos + cDrawOffset);
+	q.anchor = cAnchor;
+	q.scale = XY{ flipX ? -camera.scale : camera.scale, camera.scale } *scale;
+	q.radians = radians;
+	q.colorplus = 1;
+	q.color = { 255, 255, 255, 255 };
+}
+
+#pragma endregion
+
 #pragma region Monster1
 
-void Monster1::Init(SceneTest1* scene_, int tracksIndex_, float pointIndex_, float speed_) {
-	scene = scene_;
-	scale = cScale;
+void Monster1::Init(ItemManagerBase* im_, int tracksIndex_, float pointIndex_, float speed_) {
+	SceneItemInit(cTypeId, im_);
+	scale = { cScale, cScale };
 	radius = cRadius;
 	speed = speed_;
 	tracksIndex = tracksIndex_;
 	pointIndex = pointIndex_;
 	pos = scene->tracks[tracksIndex_][(int)pointIndex];
+	PhysAdd();
 	radians = scene->mpc.points[(int)pointIndex].radians;
 }
 
-void Monster1::Update() {
+bool Monster1::Update() {
 	auto siz = (int)scene->mpc.points.size();
 	pointIndex += speed;
 	if (pointIndex >= siz) {
 		pointIndex -= siz;
 	}
 	pos = scene->tracks[tracksIndex][(int)pointIndex];
+	PhysUpdate();
 	radians = scene->mpc.points[(int)pointIndex].radians;
+	return false;
 }
 
-void Monster1::Draw() {
-	auto& c = scene->camera;
+void Monster1::Draw(Camera const& camera) {
 	auto& q = Quad::DrawOnce(gLooper.frames_coin_2[0]);
-	q.pos = c.ToGLPos(pos);
+	q.pos = camera.ToGLPos(pos);
 	q.anchor = cAnchor;
-	q.scale = XY::Make(c.scale) * scale;
+	q.scale = XY::Make(camera.scale) * scale;
 	q.radians = radians;
 	q.colorplus = 1;
 	q.color = RGBA8_White;
@@ -50,14 +301,22 @@ void SceneTest1::Init() {
 		camera.DecreaseScale(0.1f, 0.1f);
 		});
 
+	camera.SetMaxFrameSize({ gCfg.maxItemSize, gCfg.maxItemSize });
+	camera.SetOriginal(gCfg.mapCenterPos);
 	camera.SetScale(1.f);
+
+	sgcPhysItems.Init(gCfg.physNumRows, gCfg.physNumCols, gCfg.physCellSize);
+	im.Init(this);
+
+	// create human
+	im.Create<Human>();
 
 	// curve points
 	std::vector<CurvePoint> cps{ CurvePoint
-		{ XY{ -300, -180} },
-		{ XY{ 300, -180} },
-		{ XY{ 300, 180} },
-		{ XY{ -300, 180} }
+		{ gCfg.mapCenterPos + XY{ -300, -180} },
+		{ gCfg.mapCenterPos + XY{ 300, -180} },
+		{ gCfg.mapCenterPos + XY{ 300, 180} },
+		{ gCfg.mapCenterPos + XY{ -300, 180} }
 	};
 
 	// fill path
@@ -88,10 +347,10 @@ void SceneTest1::Init() {
 	tasks.Add([this]()->xx::Task<> {
 		while (true) {
 			// create some monsters
-			if (monsters.len < 100000) {
+			if (im.Get<Monster1>().len < 100000) {
 				for (int i = 0; i < trackCount; i++) {
 					for (int j = 0; j < 10; j++) {
-						monsters.Emplace().Emplace<Monster1>()->Init(this, i
+						im.Create<Monster1>(i
 							, gLooper.rnd.Next<float>(0, 100)
 							, gLooper.rnd.Next<float>(trackBaseSpeed, trackBaseSpeed * 5));
 					}
@@ -111,15 +370,13 @@ void SceneTest1::Update() {
 	}
 	camera.Calc();
 
-	for (auto& o : monsters) {
-		o->Update();
-	}
+	im.Update();
 }
 
 void SceneTest1::Draw() {
 	camera.Calc();
 
-	// todo: draw xxxx
+	// draw track
 	LineStrip ls;
 	auto siz = (int)mpc.points.size();
 	for (int i = 0; i < siz; i += trackDrawStep) {
@@ -130,11 +387,10 @@ void SceneTest1::Draw() {
 		ls.Draw();
 	}
 
-	for (auto& o : monsters) {
-		o->Draw();
-	}
+	// draw items ( order by y )
+	im.DrawAll(camera);
 
-	auto str = xx::ToString("zoom: Z / X. monster count = ", monsters.len);
+	auto str = xx::ToString("zoom: Z / X. item count = ", im.GetSize());
 	gLooper.ctcDefault.Draw({ 0, gLooper.windowSize_2.y - 5 }, str, RGBA8_Green, { 0.5f, 1 });
 	gLooper.DrawNode(rootNode);
 };
--- d21/scene_test1.h
+++ d21/scene_test1.h
@@ -1,17 +1,125 @@
 ﻿#pragma once
 #include <game_looper.h>
 
-struct SceneTest1;
+struct Cfg {
+	static constexpr float maxItemSize{ 32 };
 
-struct MonsterBase {
-	SceneTest1* scene{};
-	XY pos{};
-	float scale{}, radius{}, radians{};
-	virtual void Update() = 0;
-	virtual void Draw() = 0;
+	static constexpr int physCellSize{ 32 };	// need >= max item size
+	static constexpr int physNumRows{ 512 };
+	static constexpr int physNumCols{ 512 };
+
+	static constexpr XY mapSize{ float(physNumCols * physCellSize), float(physNumRows * physCellSize) };
+	static constexpr XY mapSize_2{ mapSize.x / 2, mapSize.y / 2 };
+	static constexpr XY mapCenterPos{ mapSize_2 };
+	static constexpr XY mapSafeMinPos{ maxItemSize, maxItemSize };
+	static constexpr XY mapSafeMaxPos{ mapSize.x - maxItemSize, mapSize.y - maxItemSize };
+
+	static constexpr float mouseHitRange{ 50.f };
+};
+inline Cfg gCfg;
+
+
+
+struct SceneTest1;
+using SceneItem = SceneItemBase<SceneTest1>;
+
+struct SceneTest1;
+struct PhysSceneItem : SceneItem, SpaceGridCItem<PhysSceneItem, XY> {
+	void PhysAdd();				// call after fill pos
+	void PhysUpdate();			// call after fill pos
+	void PhysRemove();
+	bool PhysExists();
+	~PhysSceneItem();
 };
 
-struct Monster1 : MonsterBase {
+
+
+struct Human;
+struct Staff : SceneItem {
+	static constexpr int cTypeId{ 1 };
+	static constexpr XY cAnchor{ 0.2f, 0.5f };
+	static constexpr int cFrameIndex{ 1 };
+
+	static constexpr float cFrameMaxChangeRadians{ float(M_PI * 10 / gDesign.fps) };
+	static constexpr float cFireDelaySecs{ 0.05f };
+	static constexpr float cFireDistance{ 18 };
+
+	xx::Weak<SceneItem> owner;
+	float nextFireSecs{};
+	XY firePos{};
+
+	xx::Task<> mainTask;
+	xx::Task<> MainTask();
+
+	void Init(ItemManagerBase* im_, xx::Weak<SceneItem> owner_);
+	virtual bool Update() override;
+	virtual void Draw(Camera const& camera) override;
+};
+
+
+
+struct Bullet : SceneItem {
+	static constexpr int cTypeId{ 2 };
+
+	static constexpr float cScale{ 0.5f };
+	static constexpr XY cAnchor{ 0.5f, 0.5f };
+
+	static constexpr float cRadius{ 4 };
+	static constexpr float cSpeed{ 400.f / gDesign.fps };
+	static constexpr float cLifeSpan{ 0.25 };
+	static constexpr int cLifeNumFrames{ int(cLifeSpan / gDesign.frameDelay) };
+
+	xx::Weak<SceneItem> owner;
+	int damage{};
+	// todo: damage
+
+	xx::Task<> mainTask;
+	xx::Task<> MainTask();
+	xx::Task<> moveTask;
+	xx::Task<> MoveTask();
+
+	void Init(ItemManagerBase* im_, xx::Weak<Staff> weapon_);
+	virtual bool Update() override;
+	virtual void Draw(Camera const& camera) override;
+};
+
+
+
+struct Human : SceneItem {
+	static constexpr int cTypeId{ 3 };
+
+	static constexpr float cIdleScaleYFrom{ 0.9f };
+	static constexpr float cIdleScaleYTo{ 1.f };
+	static constexpr float cIdleScaleYStep{ (cIdleScaleYTo - cIdleScaleYFrom) * 2 / gDesign.fps };
+
+	static constexpr XY cAnchor{ 0.5f, 0 };
+	static constexpr XY cDrawOffset{ 0, 4.f };
+	static constexpr float cRadius{ 5.f };
+	static constexpr std::array<float, 5> cFrameIndexRanges{ 0.f, 3.f, 6.f, 9.f, 12.f };
+	static constexpr float cFrameInc{ 12.f / gDesign.fps };
+	static constexpr float cSpeed{ 60.f / gDesign.fps };
+
+	float speed{};
+	MoveDirections direction{};
+	float frameIndexFrom{}, frameIndexTo{};
+	void SetDirection(MoveDirections d);
+	XY GetPhysPos();
+
+	xx::Task<> mainTask;
+	xx::Task<> MainTask();
+	xx::Task<> idleTask;
+	xx::Task<> IdleTask();
+
+	void Init(ItemManagerBase* im_);
+	virtual bool Update() override;
+	virtual void Draw(Camera const& camera) override;
+};
+
+
+
+struct Monster1 : PhysSceneItem {
+	static constexpr int cTypeId{ 4 };
+
 	static constexpr XY cAnchor{ 0.5f, 0.5f };
 	static constexpr float cScale{ 1 };
 	static constexpr float cRadius{ cScale * 7 };
@@ -19,16 +127,21 @@ struct Monster1 : MonsterBase {
 	int tracksIndex{};
 	float pointIndex{}, speed{};
 
-	void Init(SceneTest1* scene_, int tracksIndex_, float pointIndex_, float speed_);
-	virtual void Update() override;
-	virtual void Draw() override;
+	void Init(ItemManagerBase* im_, int tracksIndex_, float pointIndex_, float speed_);
+	virtual bool Update() override;
+	virtual void Draw(Camera const& camera) override;
 };
 
+
+
+
 struct SceneTest1 : Scene {
+	SpaceGridC<PhysSceneItem, XY> sgcPhysItems;	// must at top
 	xx::Shared<Node> rootNode;
 	Camera camera;
-	xx::Listi32<xx::Shared<MonsterBase>> monsters;	// todo: phys
-
+	ItemManager<Staff, Bullet, Human, Monster1> im;
+	xx::Weak<Human> human;	// point to im human Item
+	bool isBorderVisible{};
 
 	static constexpr int trackCount{ 10 };
 	static constexpr int trackDrawStep{ 100 };
