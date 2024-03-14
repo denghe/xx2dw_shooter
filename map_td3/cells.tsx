be3901cac96c1c8e92ae5111dc7262963ca7a88f
denghe <denghe@hotmail.com>
2024-03-14 12:29:00 +0800

    add more td res

---
+++ map_td3/cells.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cells" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="pics/td_cell_empty.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="pics/td_cell_montain.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="pics/td_cell_road.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="pics/td_cell_space.png"/>
+ </tile>
+</tileset>
