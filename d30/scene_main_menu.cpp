3388fa3c014ba9a2d494ec5af3be4c3234f63550
denghe <denghe@hotmail.com>
2024-03-28 03:05:35 +0800

    writing...

--- d30/scene_main_menu.cpp
+++ d30/scene_main_menu.cpp
@@ -4,6 +4,204 @@
 #include "scene_test1.h"
 #include "xx_blocklink.h"
 
+namespace xx {
+
+	template<typename T = int32_t, typename SizeType = uint16_t, SizeType cap = 16>
+	struct FixedListPool;
+
+	// copy code fom List ( maybe combine with List ? )
+	template<typename T = int32_t, typename SizeType = uint16_t, SizeType cap = 16>
+	struct FixedListCore {
+		typedef T ChildType;
+		using Pool = FixedListPool<T, SizeType, cap>;
+
+		Pool* pool;
+		SizeType len;
+		T buf[0];
+
+		FixedListCore() = delete;
+		FixedListCore(FixedListCore const& o) = delete;
+		FixedListCore& operator=(FixedListCore const& o) = delete;
+		~FixedListCore() noexcept {
+			Clear();
+		}
+
+		template<typename...Args>
+		T& Emplace(Args&&...args) {
+			assert(len < cap);
+			return *new (&buf[len++]) T(std::forward<Args>(args)...);
+		}
+
+		template<typename ...TS>
+		void Add(TS&&...vs) {
+			(Emplace(std::forward<TS>(vs)), ...);
+		}
+
+		void RemoveAt(SizeType idx) {
+			assert(idx >= 0 && idx < len);
+			--len;
+			if constexpr (IsPod_v<T>) {
+				buf[idx].~T();
+				::memmove(buf + idx, buf + idx + 1, (len - idx) * sizeof(T));
+			} else {
+				for (SizeType i = idx; i < len; ++i) {
+					buf[i] = (T&&)buf[i + 1];
+				}
+				buf[len].~T();
+			}
+		}
+
+		void SwapRemoveAt(SizeType idx) {
+			assert(idx >= 0 && idx < len);
+			buf[idx].~T();
+			--len;
+			if (len != idx) {
+				if constexpr (IsPod_v<T>) {
+					::memcpy(&buf[idx], &buf[len], sizeof(T));
+				} else {
+					new (&buf[idx]) T((T&&)buf[len]);
+				}
+			}
+		}
+
+		void Clear() {
+			if (!cap) return;
+			if (len) {
+				for (SizeType i = 0; i < len; ++i) {
+					buf[i].~T();
+				}
+				len = 0;
+			}
+		}
+
+		T const& operator[](SizeType idx) const noexcept {
+			assert(idx >= 0 && idx < len);
+			return buf[idx];
+		}
+
+		T& operator[](SizeType idx) noexcept {
+			assert(idx >= 0 && idx < len);
+			return buf[idx];
+		}
+
+		struct Iter {
+			T* ptr;
+			bool operator!=(Iter const& other) noexcept { return ptr != other.ptr; }
+			Iter& operator++() noexcept { ++ptr; return *this; }
+			T& operator*() noexcept { return *ptr; }
+		};
+		Iter begin() noexcept { return Iter{ buf }; }
+		Iter end() noexcept { return Iter{ buf + len }; }
+		Iter begin() const noexcept { return Iter{ buf }; }
+		Iter end() const noexcept { return Iter{ buf + len }; }
+	};
+
+
+	// wrapper
+	template<typename T = int32_t, typename SizeType = uint16_t, SizeType cap = 16>
+	struct FixedList {
+		using Pool = FixedListPool<T, SizeType, cap>;
+		using Core = FixedListCore<T, SizeType, cap>;
+
+		Core* core{};
+
+		FixedList() = default;
+		FixedList(FixedList const& o) = delete;
+		FixedList& operator=(FixedList const& o) = delete;
+		FixedList(FixedList&& o) : core(std::exchange(o.core, nullptr)) {};
+		FixedList& operator=(FixedList&& o) {
+			swap(core, o.core);
+			return *this;
+		}
+
+		void Alloc(Pool& owner);
+
+		~FixedList() noexcept {
+			if (core) core->Clear();
+			// todo: release core from pool
+		}
+
+		template<typename...Args>
+		T& Emplace(Args&&...args) {
+			assert(core);
+			return core->Emplace(std::forward<Args>(args)...);
+		}
+
+		template<typename ...TS>
+		void Add(TS&&...vs) {
+			assert(core);
+			(core->Emplace(std::forward<TS>(vs)), ...);
+		}
+
+		void RemoveAt(SizeType idx) {
+			assert(core);
+			core->RemoveAt(idx);
+		}
+
+		void SwapRemoveAt(SizeType idx) {
+			assert(core);
+			core->SwapRemoveAt(idx);
+		}
+
+		void Clear() {
+			assert(core);
+			core->Clear();
+		}
+
+		T const& operator[](SizeType idx) const noexcept {
+			assert(core);
+			return core->operator[](idx);
+		}
+
+		T& operator[](SizeType idx) noexcept {
+			assert(core);
+			return core->operator[](idx);
+		}
+
+		Core::Iter begin() noexcept { assert(core); return core->begin(); }
+		Core::Iter end() noexcept { assert(core); return core->end(); }
+		Core::Iter begin() const noexcept { assert(core); return core->begin(); }
+		Core::Iter end() const noexcept { assert(core); return core->end(); }
+	};
+
+	template<typename T, typename SizeType, SizeType cap>
+	struct FixedListPool {
+		using Node = FixedList<T, SizeType>;
+		static constexpr size_t nodeSize = sizeof(Node) + sizeof(T) * cap;
+		static constexpr size_t blockSize = nodeSize * 64;
+
+		xx::Listi32<void*> blocks;
+		int32_t cap{}, len{}, freeHead{ -1 }, freeCount{};
+
+		XX_FORCE_INLINE void Reserve() {
+			cap += 64;
+			blocks.Emplace(malloc(blockSize));
+		}
+
+		//XX_FORCE_INLINE void* RefBlock(int32_t index) const {
+		//	assert(index >= 0 && index < this->cap);
+		//	return *(Block*)this->blocks[(uint32_t&)index >> 6];
+		//}
+
+		//XX_FORCE_INLINE Node<T>& RefNode(int32_t index) const {
+		//	auto& block = RefBlock(index);
+		//	auto& node = block.buf[index & 0b111111];
+		//	assert(node.index == index);
+		//	return node;
+		//}
+
+
+		// todo
+	};
+
+	template<typename T, typename SizeType, SizeType cap>
+	void FixedList<T, SizeType, cap>::Alloc(Pool& owner) {
+
+	}
+
+}
+
+
 void SceneMainMenu::Init() {
 	rootNode.Emplace()->Init();
 
@@ -20,29 +218,6 @@ void SceneMainMenu::Init() {
 	}, 3);
 
 
-	//Rnd rnd;
-	//int64_t counter{};
-	//xx::BlockLink<xx::Task<>> tasks;
-	//auto secs = xx::NowEpochSeconds();
-	//for (size_t i = 0; i < 200000; i++) {
-	//	tasks.Emplace(([](Rnd& rnd, int64_t& counter)->xx::Task<> {
-	//		for (int i = 0, e = rnd.Next(100, 1000);
-	//			i < e;
-	//			i++) {
-	//			++counter;
-	//			co_yield 0;
-	//		}
-	//		})(rnd, counter));
-	//}
-	//do {
-	//	//tasks.Foreach([](auto& o)->xx::ForeachResult {
-	//	tasks.ForeachLink([](auto& o)->xx::ForeachResult {
-	//		return o.Resume() ? xx::ForeachResult::RemoveAndContinue : xx::ForeachResult::Continue;
-	//	});
-	//} while (tasks.Count());
-
-	//xx::CoutN(xx::NowEpochSeconds(secs));
-	//xx::CoutN(counter);
 }
 
 void SceneMainMenu::Draw() {
